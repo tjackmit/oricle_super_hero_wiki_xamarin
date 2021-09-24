8c173b7c9d24cb17e759e075805c4217447265d4
dev <tim.d.fuller@gmail.com>
2021-09-24 15:07:28 -0600

    Tim Fuller - Update to oricle_super_hero_wiki\MainPage.xaml, oricle_super_hero_wiki\MainPage.xaml.cs, oricle_super_hero_wiki.csproj, Resource.designer.cs, MultipleResults.cs and SingleResult.cs.

--- oricle_super_hero_wiki/oricle_super_hero_wiki/MainPage.xaml.cs
+++ oricle_super_hero_wiki/oricle_super_hero_wiki/MainPage.xaml.cs
@@ -10,9 +10,52 @@ namespace oricle_super_hero_wiki
 {
     public partial class MainPage : ContentPage
     {
+        string searchValue = "";
+
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        bool IsDigitsOnly(string str)
+        {
+            foreach (char c in str)
+            {
+                if (c < '0' || c > '9')
+                    return false;
+            }
+
+            return true;
+        }
+
+        void OnButtonClicked(object sender, EventArgs args)
+        {
+            Console.WriteLine("Search button pressed....");
+
+            if (searchValue != "")
+            {
+                Console.WriteLine("searchValue: " + searchValue);
+
+                if (IsDigitsOnly(searchValue))
+                {
+                    Console.WriteLine("search for single result....");
+                }
+
+                else
+                {
+                    Console.WriteLine("search for multiple results....");
+                }
+
+            }
+        }
+
+        void Entry_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            //var oldText = e.OldTextValue;
+            //Console.WriteLine("oldText: " + oldText);
+            //var newText = e.NewTextValue;
+            //Console.WriteLine("newText: " + newText);
+            searchValue = e.NewTextValue;
         }
     }
 }
